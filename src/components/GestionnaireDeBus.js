6f5b9562db487851376c3c7e80cacffbe864f86b
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2020-10-19 13:28:59 -0400

    Préparation du component général GestionnaireDeBus, closes #1

---
+++ src/components/GestionnaireDeBus.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class GestionnaireDeBus extends React.Component {
+  
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+
+    };
+
+  }
+
+  render() {
+    return (
+      <p>Ceci est un test!</p>
+    );
+  }
+
+}
+
+export default GestionnaireDeBus
