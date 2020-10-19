9a9aa37ec67529f573c1aa7547acfa96ec0536e3
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2020-10-19 13:09:08 -0400

    installation initiale

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import GestionnaireDeBus from './components/GestionnaireDeBus';
+
+ReactDOM.render(
+  <GestionnaireDeBus />,
+  document.getElementById('container')
+);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path    = require('path');
+const webpack = require('webpack');
+const dotenv  = require('dotenv');
+
+const env = dotenv.config().parsed;
+
+// create a nice object from the env variable
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  mode: 'development',
+  plugins: [
+    new webpack.DefinePlugin(envKeys)
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  }
+};
