d30de0477fbf485ebbe4e4fbdee6c9b6c18ca623
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2020-10-26 13:17:11 -0400

    ajout de nouveaux types de inputs dans le formulaire et ajout d'une carte Mapbox

--- src/components/FormModifierBus.js
+++ src/components/FormModifierBus.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import ReactMapboxGl from "react-mapbox-gl";
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+console.log(process.env.BLABLA);
+
+const Map = ReactMapboxGl({
+  accessToken: process.env.MAPBOX_ACCESS_TOKEN
+});
 
 export default class FormModifierBus extends React.Component {
 
@@ -12,6 +20,7 @@ export default class FormModifierBus extends React.Component {
 
     this.onChangeImmatriculation = this.onChangeImmatriculation.bind(this);
     this.onChangeMarque          = this.onChangeMarque.bind(this);
+    this.onChangeCommentaires    = this.onChangeCommentaires.bind(this);
     this.onConfirmerChangements  = this.onConfirmerChangements.bind(this);
     this.onSupprimerBus          = this.onSupprimerBus.bind(this);
   }
@@ -27,6 +36,14 @@ export default class FormModifierBus extends React.Component {
   onChangeMarque(event) {
     const bus = this.state.bus;
     bus.marque = event.target.value;
+    this.setState({
+      bus
+    });
+  }
+
+  onChangeCommentaires(event) {
+    const bus = this.state.bus;
+    bus.commentaires = event.target.value;
     this.setState({
       bus
     });
@@ -64,14 +81,32 @@ export default class FormModifierBus extends React.Component {
           value={this.state.bus.immatriculation}
           onChange={this.onChangeImmatriculation}
         />
+
         <label>Marque</label>
-        <input 
+        <select
           type="text"
           id="marque"
           name="marque"
           value={this.state.bus.marque}
           onChange={this.onChangeMarque}
+        >
+          <option value="scania">Scania</option>
+          <option value="mercedes">Mercedes-Benz</option>
+          <option value="volvo">Volvo</option>
+          <option value="novabus">NovaBus</option>
+        </select>
+
+        <label>Commentaires</label>
+        <textarea
+          cols="10"
+          rows="3"
+          id="commentaires"
+          name="commentaires"
+          onChange={this.onChangeCommentaires}
         />
+
+        <Map style="mapbox://styles/mapbox/streets-v8"/>
+
         <input type="submit" id={`confirmer_bus_${this.state.bus.id}`} value="Confirmer" />
         <button onClick={this.onSupprimerBus}>Supprimer ce bus</button>
       </form>
